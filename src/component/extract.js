f36d52cc9a49a757851ae9f50a7b6312f2dd1ff0
Marco Nie <me@niekun.net>
2021-02-24 17:18:09 +0800

    update

--- src/component/extract.js
+++ src/component/extract.js
@@ -1,22 +1,33 @@
 function Comment(props) {
     return (
-      <div className="Comment">
-        <div className="UserInfo">
-          <img className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <div className="UserInfo-name">
-            {props.author.name}
-          </div>
+        <div className="Comment">
+            <UserInfo user={props.user} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
         </div>
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {formatDate(props.date)}
-        </div>
-      </div>
     );
-  }
-  
+}
+
+const Avatar = (props) => {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
+    );
+}
+
+const UserInfo = (props) => {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
