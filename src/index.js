bfd2bd4ef874f8d87ca5d5384adfce6d744d6617
Marco Nie <me@niekun.net>
2021-02-25 16:00:08 +0800

    update

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = { date: new Date() }
+        this.tick = this.tick.bind(this);
     }
 
     componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1000);
+        this.timerID = setInterval(this.tick, 1000);
     }
 
     componentWillUnmount() {
